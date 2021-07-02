911df9eb1ab374e13c0f801cae22c6756758bd76
Krzysztof Szot <satrix321@gmail.com>
2021-07-02 09:25:25 +0200

    Add basic filtering support on the homepage, without input field yet

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,11 @@
 <template>
   <MasonryContainer>
     <HandoutCard
-      title="Old Newspaper Article"
-      :image="require('@/assets/newspaper-headline.jpg')"
-      url="/handout/old-newspaper"
+      v-for="item in filteredHandoutCards"
+      :key="item.title"
+      :title="item.title"
+      :url="item.url"
+      :image="item.image"
       width="300"
       class="masonry-item"
     />
@@ -11,15 +13,47 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, ref, computed } from "vue";
 import MasonryContainer from "@/components/MasonryContainer.vue";
 import HandoutCard from "@/components/HandoutCard.vue";
+
+type HandoutCardRecord = {
+  title: string;
+  url: string;
+  image: unknown;
+};
 
 export default defineComponent({
   name: "Home",
   components: {
     MasonryContainer,
     HandoutCard,
+  },
+  setup() {
+    const filterValue = ref<string>("");
+    const handoutCards = ref<HandoutCardRecord[]>([
+      {
+        title: "Newspaper Article",
+        url: "/handout/newspaper",
+        image: require("@/assets/newspaper-headline.jpg"),
+      },
+      {
+        title: "Old Newspaper Article",
+        url: "/handout/old-newspaper",
+        image: require("@/assets/newspaper-headline.jpg"),
+      },
+    ]);
+
+    const filteredHandoutCards = computed(() => {
+      return handoutCards.value.filter((item) =>
+        item.title.toLowerCase().includes(filterValue.value.toLowerCase())
+      );
+    });
+
+    return {
+      filterValue,
+      filteredHandoutCards,
+    };
   },
 });
 </script>
