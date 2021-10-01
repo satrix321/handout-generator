0b0c402d1c0c3db95cffddea385775a57c42c7f8
Krzysztof Szot <satrix321@gmail.com>
2021-10-01 11:48:35 +0200

    Updates

--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import "tailwindcss/tailwind.css";
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
-import "./styles/fonts.scss";
-
-createApp(App).use(store).use(router).mount("#app");
