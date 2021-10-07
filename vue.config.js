f68ba91c162d96203b25927d7f2f3e423a4192a9
Krzysztof Szot <satrix321@gmail.com>
2021-10-07 15:36:28 +0200

    Generate source maps on DEV build

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 module.exports = {
   publicPath:
     process.env.NODE_ENV === "production" ? "/handout-generator/" : "/",
+  configureWebpack: (config) => {
+    if (process.env.NODE_ENV === "development") {
+      config.devtool = "source-map";
+    }
+  },
 };
